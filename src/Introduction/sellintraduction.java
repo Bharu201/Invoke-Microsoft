47c9615b9a4ff8d95c98f56d3a0b9720a2c8607b
Chagantibhargavi <98128875+Chagantibhargavi@users.noreply.github.com>
2024-03-18 12:43:39 -0400

    First commit

---
+++ src/Introduction/sellintraduction.java
@@ -0,0 +1,24 @@
+package Introduction;
+
+//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.*;
+public class sellintraduction {
+
+	public static void main(String[] args) {
+		
+		// Invoking Browser
+		// MicrosoftEdge- Edge Driver ->Methods
+		
+		//System.setProperty("webdriver.edge.driver", "D:\\eclipse-java-2023-12-R-win32-x86_64edgedriver_win64\\msedgedriver.exe");
+		EdgeDriver driver = new EdgeDriver();
+		
+		
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		//driver.close();
+		//driver.quit();
+	}
+
+}
